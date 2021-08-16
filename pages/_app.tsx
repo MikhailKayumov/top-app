9896a9382b54c84d17f60d5de143328f5231addd
Каюмов Михаил Линарович <MLKayumov@sberbank.ru>
2021-08-16 15:15:39 +0500

    1) create custom document; 2) remove standard layout; 3) add colors and fonts;

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,19 @@
-import {AppProps} from "next/app";
+import { AppProps } from "next/app";
+import Head from "next/head";
 
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>MyTop</title>
+        <meta name="description" content="Топ лучших IT и дизайн курсов" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
