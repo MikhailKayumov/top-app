cd29796fd8e078a0015ae6ce2e0e61b2b66ea42e
Каюмов Михаил Линарович <MLKayumov@sberbank.ru>
2021-08-18 14:08:02 +0500

    add styles to layout

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import clsx from "classnames";
+import { format } from 'date-fns';
 
 import { FooterProps } from './Footer.props';
 import styles from './Footer.module.css';
 
-export const Footer: React.FC<FooterProps> = (props): JSX.Element => {
+export const Footer: React.FC<FooterProps> = ({ className, ...props }): JSX.Element => {
   return (
-    <div {...props}>Footer</div>
+    <footer className={clsx(styles.footer, className)} {...props}>
+      <div>
+        MyTop © 2020 - {format(new Date, 'yyyy')} Все права защищены
+      </div>
+      <a href="#" target="_blank">
+        Пользовательское соглашение
+      </a>
+      <a href="#" target="_blank">
+        Политика конфиденциальности
+      </a>
+    </footer>
   );
 };
